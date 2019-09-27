0ce316c68c1c75e3f0febe9a481677700d63e7b8
davalapar <davalapar@gmail.com>
2019-09-27 09:53:21 +0800

    nc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-bitwise */
 
 const crypto = require('crypto');
 const base32 = require('hi-base32');
@@ -44,8 +44,38 @@ const totp = (secret) => {
   return (parseInt(truncated, 16) % 1000000).toString().padStart(6, '0');
 };
 
-const code = totp('PFQW233NONQWQ33F');
-console.log({ code });
+const code2 = totp('PFQW233NONQWQ33F');
+console.log({ code2 });
+
+
+const hotp = (key, counter) => {
+  // message := floor(current Unix time / 30)
+  const message = Buffer.alloc(8).fill(0);
+  message.writeUInt32BE(counter, 4);
+
+  // hash := HMAC-SHA1(secret, message)
+  const secret = Buffer.from(key);
+  const hash = crypto.createHmac('sha1', secret).update(message).digest();
+
+  // offset := last nibble of hash
+  const offset = hash[19] & 0xf;
+
+  // truncatedHash := hash[offset..offset+3]  //4 bytes starting at the offset
+  // Set the first bit of truncatedHash to zero  //remove the most significant bit
+  let code = (hash[offset] & 0x7f) << 24;
+  code |= (hash[offset + 1] & 0xff) << 16;
+  code |= (hash[offset + 2] & 0xff) << 8;
+  code |= (hash[offset + 3] & 0xff);
+
+  // code := truncatedHash mod 1000000
+  code %= 1000000;
+
+  // pad code with 0 from the left until length of code is 6
+  code = String(code).padStart(6, '0');
+  return code;
+};
+const timestamp = Math.floor(Math.round(Date.now() / 1000) / 30);
+console.log(hotp(base32.decode('PFQW233NONQWQ33F'), timestamp));
 
 //
 /*
